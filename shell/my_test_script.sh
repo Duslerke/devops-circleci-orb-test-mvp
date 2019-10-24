2d03fed65ab970a12baa61839a5e2ca6dbc7d27d
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2019-10-24 15:09:01 +0100

    Let's break CCI with << operator from inserted code
    and develop a way to combat it

--- shell/my_test_script.sh
+++ shell/my_test_script.sh
@@ -8,6 +8,11 @@ function spamTheScreen() {
     done
 }
 
+#Oh btw, this code contains this operator << omg, circle ci will break now
+#Unless I escape << it with \<<, this requires automation, however.
+#And escaping it in code, will prob just make the shell crash, when you test it anyway!!!
+#Let's see it break, if it doesn't I'll add an actual operator
+
 spamTheScreen 0 100
 
 #>>Script1<<#
